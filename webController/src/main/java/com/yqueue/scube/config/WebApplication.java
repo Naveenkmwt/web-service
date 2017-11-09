11d394077cee805a2de8c6961f189a363eda8986
Naveen kumawat <naveendambiwal@gmail.com>
2017-11-09 13:00:38 +0530

    Y-2 User entity created and updated package name

--- webController/src/main/java/com/yqueue/scube/config/WebApplication.java
+++ webController/src/main/java/com/yqueue/scube/config/WebApplication.java
@@ -1,4 +1,4 @@
-package com.yqueue.scube.auth;
+package com.yqueue.scube.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
